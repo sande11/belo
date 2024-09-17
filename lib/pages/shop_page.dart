52c1277cd66972eecea8256bbe02d3abb84153f6
sande11 <kelvinsande9@gmail.com>
2024-09-17 23:30:21 +0200

    search bar

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -5,8 +5,27 @@ class ShopPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('shop'),
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
+            color: Colors.grey[100],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Search'),
+              Icon(
+                Icons.search,
+                color: Colors.grey,
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
