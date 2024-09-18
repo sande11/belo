9a8c02ae1ed0a36d5328ca947b1201fb88b0726f
sande11 <kelvinsande9@gmail.com>
2024-09-18 18:46:14 +0200

    hotpics

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,3 +1,5 @@
+import 'package:belo/components/shoe_tile.dart';
+import 'package:belo/models/shoe.dart';
 import 'package:flutter/material.dart';
 
 class ShopPage extends StatelessWidget {
@@ -23,6 +25,62 @@ class ShopPage extends StatelessWidget {
                 color: Colors.grey,
               )
             ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Text(
+            'Fashions fade, style is eternal',
+            style: TextStyle(color: Colors.grey[600]),
+          ),
+        ),
+
+        // top picks
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                'Top Picks',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+              Text(
+                'see all',
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),
+              )
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+
+        Expanded(
+            child: ListView.builder(
+                itemCount: 4,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  Shoe shoe = Shoe(
+                      name: 'Air Jordan',
+                      price: 'K54,000',
+                      imagePath: 'assets/images/air jordan red.jpg',
+                      description: 'cool shoe');
+
+                  return ShoeTile(
+                    // create a shoe
+                    shoe: shoe,
+                  );
+                })),
+        const Padding(
+          padding: EdgeInsets.only(top: 25.0, left: 25, right: 25),
+          child: Divider(
+            color: Colors.white,
           ),
         )
       ],
