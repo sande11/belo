c5f1cafe1c913d1cf86e601f9342edd304776f9f
sande11 <kelvinsande9@gmail.com>
2024-09-21 11:32:29 +0200

    cart

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,12 +1,35 @@
+import 'package:belo/components/cart_item.dart';
+import 'package:belo/models/cart.dart';
+import 'package:belo/models/shoe.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('cart'),
-    );
+    return Consumer<Cart>(
+        builder: (context, value, child) => Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Column(
+                children: [
+                  const Text(
+                    'My Cart',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+
+                  Expanded(child: ListView.builder(
+                    itemCount: value.getShoeList().length,
+                    itemBuilder: (context, index){
+                    Shoe individualShoe = value.getShoeList()[index];
+                    return CartItem(shoe: individualShoe);
+                  },))
+                ],
+              ),
+            ));
   }
 }
